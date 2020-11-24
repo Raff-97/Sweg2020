8b5994175eb8e295bfc9c7955d7dde6697d34a04
Raff-97 <raffcrisci@gmail.com>
2020-11-24 15:20:15 +0100

    modificofilehellodamaster

--- myfile.c
+++ myfile.c
@@ -1,6 +1,7 @@
 int main(){
 	println(" ciao nuovo branch, effettuo la modifica sul branch ciao ");
 	println("Sto modificando il file da branch 2");
+	println("sto modificando il file dal branch GUI");
 }
 
 
